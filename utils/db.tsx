cfb247df088e8d715d66626fa4e1678a15f423df
Saurabh Prakash <saurabhp75@gmail.com>
2024-03-18 08:41:23 +0530

    Cleanup the code

---
+++ utils/db.tsx
@@ -0,0 +1,27 @@
+
+import { remember } from '@epic-web/remember'
+import { PrismaClient } from '@prisma/client'
+
+
+export const prisma = remember('prisma', () => {
+    // NOTE: if you change anything in this function you'll need to restart
+	// the dev server to see your changes.
+	const client = new PrismaClient()
+	client.$connect()
+	return client
+})
+
+type SendMessageParams = { email: string, message: string }
+
+export async function sendMessage(data: SendMessageParams): Promise<{ sent: string|null }> {
+  const win = Math.random() > 0.5
+
+  if(win) return {sent: null}
+
+  const post = await prisma.post.create({
+    data: {title: data.email, content: data.message}
+  })
+
+  return {sent: post.title}
+
+}
